a4c6668be81d46a65567e73f71c25da2aed38d69
Tom Shih <tshih98@gmail.com>
2020-07-10 11:46:09 -0700

    Added server side

---
+++ server/index.js
@@ -0,0 +1,116 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Text, Decimal, Checkbox, Password, Url, Select } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const initialiseData = require('./initial-data');
+const Stars = require('./fields/Stars');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+
+const PROJECT_NAME = 'cms-proj';
+const adapterConfig = { mongoUri: 'mongodb://localhost/cms-proj' };
+
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(adapterConfig),
+  onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
+});
+
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
+
+  // Instead of a boolean, you can return a GraphQL query:
+  // https://www.keystonejs.com/api/access-control#graphqlwhere
+  return { id: user.id };
+};
+
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
+
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
+
+keystone.createList('User', {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true,
+    },
+    isAdmin: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin,
+      },
+    },
+    password: {
+      type: Password,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+  config: {
+    identityField: 'username', // default: 'email'
+    secretField: 'password', // default: 'password'
+  },
+});
+
+keystone.createList('Product', {
+  fields: {
+    name: {
+      type: Text,
+      isUnique: true
+    },
+    price_in_usd: {type: Decimal},
+    quality: { type: Stars, starCount: 5 },
+    origin: {type: Select, options: 'opt1, opt2, opt3'},
+    info: {type: Text, isMultiline: true},
+    image: {type: Url},
+  },
+});
+
+keystone.createList('Story',{
+  fields: {
+    title:{
+      type: Text, 
+      isUnique: true
+    },
+    category:{type: Select, options: 'opt1, opt2, opt3'},
+    story_content: {type: Text, isMultiline: true},
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      enableDefaultRoute: true,
+      authStrategy,
+    }),
+  ],
+};
+
+
+
