0ae93c424944c1b541fec9ef4f25dca1b9a69080
Samuel Liu <sliu055@ucr.edu>
2020-07-16 14:50:02 -0700

    "minor ui changes"

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,9 +17,15 @@ export default function HomePage() {
       </Container>
       </div>
       <div className="home-banner-2">
+        <h2>Popular Stones</h2>
+        <button /*onClick*/>product example 1</button>
+        <button>product example 2</button>
+        <button>product example 3</button>
+        <button>product example 4</button>
 
       </div>
       <div className="home-banner-3">
+        <h2>mission statement/blog post</h2>
 
       </div>
     </div>
