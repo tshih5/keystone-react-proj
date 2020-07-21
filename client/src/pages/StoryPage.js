2edb20879d9de5c164edfe0846324cb8e0f81b3c
Tom Shih <tshih98@gmail.com>
2020-07-21 13:57:27 -0700

    add formatting to story page

--- client/src/pages/StoryPage.js
+++ client/src/pages/StoryPage.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Spinner } from "react-bootstrap";
+import { Spinner, Container, Row, Col } from "react-bootstrap";
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import Story from "./../components/story";
 
 export default function StoryPage() {
   return (
-    <RenderStories />
+    <Container fluid={true}>
+      <Row xl={3} md={3} sm={1}>
+        <Col xl={8} md={7} sm={12}><RenderStories /></Col>
+        <Col xl={1} md={1} sm={12}></Col>
+        <Col xl={3} md={4} sm={12}>COLUMN 2</Col>
+      </Row>
+    </Container>
+    
   );
 }
 
