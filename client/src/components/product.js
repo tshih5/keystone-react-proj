3874423b27fc54163ca220be0ec8a719e0a44976
Tom Shih <tshih98@gmail.com>
2020-07-23 11:52:04 -0700

    show sub-category on product card

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -8,9 +8,9 @@ export default function Product(props) {
       <Card.Img variant="top" src="https://via.placeholder.com/200x200" />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
-        <Card.Text>${props.price_in_usd}</Card.Text>
+        <Card.Text>{props.price_in_usd}</Card.Text>
+        <Card.Text>Sub-category: {props.sub_category.name}</Card.Text>
       </Card.Body>
     </Card>
-    
   );
 }
