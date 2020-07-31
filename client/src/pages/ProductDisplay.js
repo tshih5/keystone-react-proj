7fd8efe8cbcbafc8015e33043646196a6541e607
Tom Shih <tshih98@gmail.com>
2020-07-31 13:45:48 -0700

    added second column

--- client/src/pages/ProductDisplay.js
+++ client/src/pages/ProductDisplay.js
@@ -5,10 +5,16 @@ import { withRouter } from "react-router-dom";
 
 function ProductDisplay(props) {
   return (
-    <div>
-      <h1>Hello from Product Display</h1>
-      <p>ProductID: {props.match.params.productid}</p>
-    </div>
+    <Container fluid={true}>
+      <Row xl={3} md={3} sm={1}>
+        <Col xl={8} md={7} sm={12}>
+          <h1>Hello from Product Display</h1>
+          <p>ProductID: {props.match.params.productid}</p>
+        </Col>
+        <Col xl={1} md={1} sm={12}></Col>
+        <Col xl={3} md={4} sm={12}>COLUMN 2</Col>
+      </Row>
+    </Container>
   );
 }
 export default withRouter(ProductDisplay);
