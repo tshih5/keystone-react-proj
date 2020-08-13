c5bd526bacb87b1d9df1990caaf0c4d59f8bf36d
Tom Shih <tshih98@gmail.com>
2020-08-13 13:30:08 -0700

    add placeholder image for items with no image

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -10,7 +10,7 @@ function Product(props) {
   let match = useRouteMatch();
   return(
     <Card border="dark" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={`./../public/${props.id}_m.webp`} />
+      <Card.Img variant="top" src={props.thumbnail == null ? 'http://placehold.jp/150x100.png': `http://localhost:3000/images/${props.thumbnail.filename}`} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>Price: {props.price_in_usd}
