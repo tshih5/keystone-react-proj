01ba07e853a69c8267002bff395b2ec37a241c86
Tom Shih <tshih98@gmail.com>
2020-08-18 12:40:25 -0700

    Add listgroup nav with category fetching

--- client/src/pages/StoryDisplay.js
+++ client/src/pages/StoryDisplay.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import { Container, Row, Col } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { withRouter, Link } from "react-router-dom";
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 
@@ -15,12 +15,17 @@ function StoryDisplay(props) {
       <Col xl={8} md={7} sm={12}>
         <GetStoryData storyID={storyID} setStoryData={setStoryData} />
         <h2>{storyData.title}</h2>
-        <img src={storyData.main_image == null ? '': `http://localhost:3000/images/${storyData.main_image.filename}`} />
+        <img className="img-fluid" src={storyData.main_image == null ? '': `http://localhost:3000/images/${storyData.main_image.filename}`} alt="" />
         <p>Date Published: {storyData.date_published}</p>
         <div dangerouslySetInnerHTML={createMarkup(storyData.story_content)} />
       </Col>
-      <Col xl={1} md={1} sm={12}></Col>
-      <Col xl={3} md={4} sm={12}>COLUMN 2</Col>
+      <Col className="d-sm-none d-md-block" xl={1} md={1} sm={12}></Col>
+      <Col xl={3} md={4} sm={12}>
+        <h3>趣聞雜談</h3>
+        <ListGroup variant="flush">
+          <StoryCategoryList />
+        </ListGroup>
+      </Col>
     </Row>
   </Container>
   );
@@ -51,6 +56,27 @@ function GetStoryData({storyID, setStoryData}){
 
   if(error) return <p>Error :(</p>
   return null;
+}
+
+function StoryCategoryList(){
+  const { loading, error, data } = useQuery(gql`
+    {
+      allStoryCategories{
+        topic
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  //console.log(data);
+  
+  return data.allStoryCategories.map((category) => (
+    //TODO: if category name contains spaces/ starting/trailing spaces, trim value and replace spaces with a "-" or ""
+    /*does not account for spaces in the category name, may cause URL issues */
+    <ListGroup.Item><Link to={`/stories/${category.topic.trim().replace(/\s/g, '-')}`}>{category.topic}</Link></ListGroup.Item>
+  ));
 }
 
 function createMarkup(story_content){
--- client/src/pages/StoryPage.js
+++ client/src/pages/StoryPage.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Spinner, Container, Row, Col } from "react-bootstrap";
+import { Spinner, Container, Row, Col, ListGroup} from "react-bootstrap";
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import Story from "./../components/story";
 import {
+  Link,
   withRouter,
 } from "react-router-dom";
 
@@ -12,11 +13,18 @@ function StoryPage(props) {
     <Container fluid={true}>
       <Row xl={3} md={3} sm={1}>
         <Col xl={8} md={7} sm={12}>
-          <div className="header"><h1>{props.match.params.topic}</h1></div>
+          <div className="story-header text-center">
+            <h1>{props.match.params.topic.replace(/-/g, ' ')}</h1>
+          </div>
           <RenderStories {...props}/>
         </Col>
-        <Col xl={1} md={1} sm={12}></Col>
-        <Col xl={3} md={4} sm={12}>COLUMN 2</Col>
+        <Col className="d-sm-none d-md-block" xl={1} md={1} sm={12}></Col>
+        <Col xl={3} md={4} sm={12}>
+          <h3>趣聞雜談</h3>
+          <ListGroup variant="flush">
+            <StoryCategoryList />
+          </ListGroup>
+        </Col>
       </Row>
     </Container>
     
@@ -50,4 +58,24 @@ function RenderStories(props){
   ));
 }
 
+function StoryCategoryList(){
+  const { loading, error, data } = useQuery(gql`
+    {
+      allStoryCategories{
+        topic
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  //console.log(data);
+  
+  return data.allStoryCategories.map((category) => (
+    //TODO: if category name contains spaces/ starting/trailing spaces, trim value and replace spaces with a "-" or ""
+    /*does not account for spaces in the category name, may cause URL issues */
+    <ListGroup.Item><Link to={`/stories/${category.topic.trim().replace(/\s/g, '-')}`}>{category.topic}</Link></ListGroup.Item>
+  ));
+}
 export default withRouter(StoryPage);
