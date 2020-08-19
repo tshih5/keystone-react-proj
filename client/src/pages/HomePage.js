64afd53c99bf14ec24cf2c09c6e9d89cdc364400
Tom Shih <tshih98@gmail.com>
2020-08-19 14:38:13 -0700

    changed css for homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -11,18 +11,37 @@ function HomePage() {
   return (
     <div>
       <div className="home-banner-1">
-        <img src={logo} className="center"/>
+        <Container>
+          <Row>
+            <Col xl={{span: 10, offset: 1}} lg={12}>
+              <img src={logo} className="main-img img-fluid"/>
+            </Col>
+          </Row>
+        </Container>
+        
       </div>
       <div className="home-banner-2">
-        <div className="text-center">
-          <h2>石頭種類</h2>
-        </div>
-        <Link to={`/products/壽山石`}>
-          <button className="ButtonProduct1">壽山石</button>
-        </Link>
-        <Link to={`/products/和田玉`}>
-          <button className="ButtonProduct2">和田玉</button>
-        </Link>
+        <Container>
+          <Row>
+            <Col>
+              <h2 className="text-center">石頭種類</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col>
+              <Link to={`/products/壽山石`}>
+                <button className="ButtonProduct1">壽山石</button>
+              </Link>
+            </Col>
+            <Col>
+              <Link to={`/products/和田玉`}>
+                <button className="ButtonProduct2">和田玉</button>
+              </Link>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
         {/* 
         <Link to={`/products/翡翠`}>
           <button className="ButtonProduct3">翡翠</button>
@@ -31,11 +50,11 @@ function HomePage() {
           <button className="ButtonProduct4">product example 4</button>
         </Link>
         */}
-        
-
       </div>
       <div className="home-banner-3">
-        <h2>mission statement/blog post</h2>
+        <div className="text-center">
+          <h2>mission statement/blog post</h2>
+        </div>
           <div className="customerStory">
             <h6>"Wow this is a really great stone i would 10/10 buy"</h6>
           </div>
