7da5b4505bb7c22f22c196d673c203d71674f75b
Tom Shih <tshih98@gmail.com>
2020-08-24 15:23:09 -0700

    added some css styles

--- client/src/components/story.js
+++ client/src/components/story.js
@@ -9,15 +9,12 @@ import {
 function Story(props) {
   let match = useRouteMatch();
   return(
-    <Jumbotron fluid>
-      <Container>
-        <img className="img-fluid" src={props.main_image == null ? '': `http://localhost:3000/images/${props.main_image.filename}`}  alt=""/>
-        {/*TODO: Scale image such that it does not extend outside the bounds of the jumbotron */}
+    <Jumbotron className="story-preview">
+        {props.main_image ? (<img className="img-fluid" src={`http://localhost:3000/images/${props.main_image.filename}`}  alt=""/>) : ''}
         <h3 className="list-group-item-heading">{props.title}</h3>
         <Link to={`${match.url}/${props.id}`}>
           <Button>View Story</Button>
         </Link>
-      </Container>
     </Jumbotron>
   );
 }
