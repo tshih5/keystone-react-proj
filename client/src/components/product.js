01fe0c1e6f4d22d140f7c3bcac9dbcd9a6f539b9
Tom Shih <tshih98@gmail.com>
2020-08-31 14:12:45 -0700

    removed unnecessary info from product grid

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, ListGroup, ListGroupItem} from "react-bootstrap";
 import {
   Link,
   useRouteMatch,
@@ -11,12 +11,9 @@ function Product(props) {
   return(
     <Card className="card-child" border="dark">
       <Card.Img variant="top" src={props.thumbnail == null ? 'http://placehold.jp/150x100.png': `${props.thumbnail.publicUrl}`} />
+      
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
-        <Card.Text>Price: {props.price_in_usd}
-          <br /> 
-          Sub-category: {props.sub_category == null ? 'No subCategory': props.sub_category.name}
-        </Card.Text>
       </Card.Body>
       <Card.Footer className="text-center">
         <Link to={`${match.url}/${props.id}`}>
