8aa1442de817e55741f226a4090727995e772475
Tom Shih <tshih98@gmail.com>
2020-08-31 13:47:16 -0700

    changed image urls to originate from s3 bucket

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -10,7 +10,7 @@ function Product(props) {
   let match = useRouteMatch();
   return(
     <Card className="card-child" border="dark">
-      <Card.Img variant="top" src={props.thumbnail == null ? 'http://placehold.jp/150x100.png': `http://localhost:3000/images/${props.thumbnail.filename}`} />
+      <Card.Img variant="top" src={props.thumbnail == null ? 'http://placehold.jp/150x100.png': `${props.thumbnail.publicUrl}`} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>Price: {props.price_in_usd}
--- client/src/components/story.js
+++ client/src/components/story.js
@@ -10,7 +10,7 @@ function Story(props) {
   let match = useRouteMatch();
   return(
     <Jumbotron className="story-preview">
-        {props.main_image ? (<img className="img-fluid" src={`http://localhost:3000/images/${props.main_image.filename}`}  alt=""/>) : ''}
+        {props.main_image ? (<img className="img-fluid" src={`${props.main_image.publicUrl}`}  alt="story image"/>) : ''}
         <h2 className="list-group-story-heading">{props.title}</h2>
         <Link to={`${match.url}/${props.id}`}>
           <Button className="sp-button">閱讀更多</Button>
